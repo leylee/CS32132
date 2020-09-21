976827d32008e592c10185d2f15ba2849d61e905
leylee <leylee2001@gmail.com>
2020-09-21 18:12:06 +0800

    Implement the k-th last element

--- 9.17/1.cpp
+++ 9.17/1.cpp
@@ -3,14 +3,10 @@
 using namespace std;
 
 template <class T>
-struct Node
-{
+struct Node {
     T data;
-    Node *next;
-    Node()
-    {
-        next = nullptr;
-    }
+    Node* next;
+    Node() { next = nullptr; }
 
     Node(T t)
     {
@@ -20,84 +16,67 @@ struct Node
 };
 
 template <class T>
-struct List
-{
-
+struct List {
     typedef Node<T> NodeT;
-    typedef NodeT *NodeTptr;
+    typedef NodeT* NodeTptr;
     int size;
     NodeTptr headptr;
 
     List()
     {
         headptr = new NodeT();
-        tailptr = new NodeT();
         headptr->next = headptr;
         size = 0;
     }
 
-    NodeTptr push_front(T t)
-    {
-        insert(headptr);
-        return nodeptr;
-    }
+    NodeTptr push_front(T t) { return insert(headptr, t); }
 
     NodeTptr insert(NodeTptr indexptr, T t)
     {
-        if (indexptr == headptr)
-            return nullptr;
+        ++size;
         NodeTptr newptr = new NodeT(t);
         newptr->next = indexptr->next;
         indexptr->next = newptr;
         return newptr;
     }
 
-    NodeTptr begin()
-    {
-        return headptr->next;
-    }
+    NodeTptr begin() { return headptr->next; }
 
-    NodeTptr end()
-    {
-        return headptr;
-    }
+    NodeTptr end() { return headptr; }
+
+    NodeTptr pop_front() { return }
 };
 
 typedef int Type;
 typedef Node<Type> NodeT;
 typedef NodeT* NodeTptr;
 
-void solve(int k, List<int> &list)
+void solve(int k, List<int>& list)
 {
     NodeTptr pt1, pt2;
     bool illegal;
 
+    // find the k-th last element
     pt1 = pt2 = list.begin();
     illegal = false;
-    for (int i = 0; i < k; ++i)
-    {
-        if (pt2 == list.end())
-        {
+    for (int i = 0; i < k; ++i) {
+        if (pt2 == list.end()) {
             illegal = true;
             break;
         }
         pt2 = pt2->next;
     }
-    if (illegal)
-    {
+    if (illegal) {
         cout << "The value k is illegal" << endl;
-    }
-    else
-    {
-        while (pt2 != list.end())
-        {
+    } else {
+        while (pt2 != list.end()) {
             pt1 = pt1->next;
             pt2 = pt2->next;
         }
         cout << "The k-th last element is " << pt1->data << " @" << pt1 << endl;
     }
-    
-    
+
+    // find the element in the middle
 }
 
 int main()
@@ -105,8 +84,7 @@ int main()
     List<int> list;
     int n, k;
     cin >> n >> k;
-    for (int i = 0; i < n; ++i)
-    {
+    for (int i = n - 1; i >= 0; --i) {
         list.push_front(Type(i));
     }
 
