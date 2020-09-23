0ca302e4e34390bc79023e403b7b74be8bbc0371
leylee <leylee2001@gmail.com>
2020-09-23 15:10:43 +0800

    Update 1.2.cpp

---
+++ 1.2.cpp
@@ -0,0 +1,9 @@
+/**
+ * @author: 1190501001 李恩宇
+ * @build_command: g++ -std=c++11 path/to/source.cpp
+ * @description: 有一个包括 100 个元素的数组, 每个元素的值都是实数,
+ * 请写出求最大元素的值及其位置的算法, 讨论它可能采取的数据结构.
+ * @input:
+ * @output:
+ * @hint: solve 函数是解题核心函数.
+ */
