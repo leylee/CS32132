79daa44d7154b34890f105cb8e5678e365803725
leylee <leylee2001@gmail.com>
2020-09-24 08:39:20 +0800

    update 1.2

--- 1.2.cpp
+++ 1.2.cpp
@@ -3,7 +3,48 @@
  * @build_command: g++ -std=c++11 path/to/source.cpp
  * @description: 有一个包括 100 个元素的数组, 每个元素的值都是实数,
  * 请写出求最大元素的值及其位置的算法, 讨论它可能采取的数据结构.
- * @input:
- * @output:
+ * @input: a line contains 100 real numbers
+ * @output: first output the array consists of all elements; then output the
+ * maximum element and its position.
  * @hint: solve 函数是解题核心函数.
  */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const int n = 100;
+
+void solve(const vector<double>& array) {
+  double maxValue = array[0];
+  int maxPosition = 0;
+
+  for (int i = 1; i < array.size(); ++i) {
+    if (array[i] > maxValue) {
+      maxValue = array[i];
+      maxPosition = i;
+    }
+  }
+
+  cout << "The maximum value is " << maxValue << " @" << maxPosition << endl;
+}
+
+int main() {
+  vector<double> array;
+
+  for (int i = 0; i < n; ++i) {
+    double value;
+    cin >> value;
+    array.push_back(value);
+  }
+
+  cout << "The array is:" << endl;
+  for (auto i = array.begin(); i != array.end(); ++i) {
+    cout << *i << ' ';
+  }
+  cout << endl;
+
+  solve(array);
+  return 0;
+}
---
+++ 1.2.in.gen.py
@@ -0,0 +1,5 @@
+import random
+
+for i in range(100):
+    print(round(random.random() * 100, 2), end=' ')
+print()
