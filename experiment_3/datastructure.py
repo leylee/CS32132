1cd134599057e5d9d07695c9e01669db8c30859d
leylee <leylee2001@gmail.com>
2020-11-19 16:26:09 +0800

    add MyStack

--- experiment_3/datastructure.py
+++ experiment_3/datastructure.py
@@ -24,7 +24,7 @@ class MyQueue:
     def pop(self):
         if len(self):
             self.size -= 1
-            node = self.head.next;
+            node = self.head.next
             self.head.next = node.next
             node.next.prev = self.head
             return node.value
@@ -34,5 +34,38 @@ class MyQueue:
     def front(self):
         if len(self):
             return self.head.next.value
+        else:
+            return None
+
+
+class MyStack:
+    def __init__(self):
+        self.size: int = 0
+        self.head: MyNode = MyNode()
+        self.head.next = self.head
+        self.head.prev = self.head
+
+    def __len__(self) -> int:
+        return self.size
+
+    def push(self, element) -> None:
+        self.size += 1
+        new_node = MyNode(element, self.head.prev, self.head)
+        self.head.prev.next = new_node
+        self.head.prev = new_node
+
+    def pop(self):
+        if len(self):
+            self.size -= 1
+            node = self.head.prev
+            node.prev.next = self.head
+            self.head.prev = node.prev
+            return node.value
+        else:
+            return None
+
+    def top(self):
+        if len(self):
+            return self.head.prev.value
         else:
             return None
